f7e32c3fe6f6d12b919810e85e7fcdda92e56e8b
Rahul B <rahulbot@gmail.com>
2017-08-01 20:30:17 -0400

    fix word2vec download script

--- scripts/download_word2vec_model.py
+++ scripts/download_word2vec_model.py
@@ -1,12 +1,13 @@
 import os
 import urllib
-from server.models import MODEL_DIR, MODEL_GOOGLE_NEWS
 
 MODEL_GOOGLE_NEWS_URL = "https://dl.dropboxusercontent.com/u/466924777/GoogleNews-vectors-negative300.bin"
 
-embeddings_file_path = os.path.join(MODEL_DIR, MODEL_GOOGLE_NEWS)
+embeddings_file_path = os.path.join('models', 'GoogleNews-vectors-negative300.bin')
 
 if not os.path.isfile(embeddings_file_path):
     print "Google word2vec model not found, downloading file..."
     urllib.urlretrieve(MODEL_GOOGLE_NEWS_URL, embeddings_file_path)
     print "  done!"
+else:
+    print "Google word2vec model already exists."
