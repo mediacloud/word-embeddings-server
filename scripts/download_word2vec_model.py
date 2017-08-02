575fd92a7f5f447a85e4be47518856727148f31d
Rahul B <rahulbot@gmail.com>
2017-08-01 20:36:28 -0400

    use absolute path for file downoad

--- scripts/download_word2vec_model.py
+++ scripts/download_word2vec_model.py
@@ -3,7 +3,8 @@ import urllib
 
 MODEL_GOOGLE_NEWS_URL = "https://dl.dropboxusercontent.com/u/466924777/GoogleNews-vectors-negative300.bin"
 
-embeddings_file_path = os.path.join('models', 'GoogleNews-vectors-negative300.bin')
+base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+embeddings_file_path = os.path.join(base_dir, 'models', 'GoogleNews-vectors-negative300.bin')
 
 if not os.path.isfile(embeddings_file_path):
     print "Google word2vec model not found, downloading file..."
