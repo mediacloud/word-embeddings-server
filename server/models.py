4e25dd31083a1bd0662297357c4f9509055c4c22
Rahul B <rahulbot@gmail.com>
2017-10-23 16:07:25 -0400

    support listing models and debug web UI

--- server/models.py
+++ server/models.py
@@ -7,10 +7,22 @@ from server import base_dir
 MODEL_DIR = "models"
 
 MODEL_GOOGLE_NEWS = "GoogleNews-vectors-negative300.bin"
+MODEL_GOOGLE_NEWS_SHORTNAME = "GoogleNews-vectors-negative300"
 
 logger = logging.getLogger(__name__)
 
 model_cache = {}  # keyed by model name
+
+
+def model_name_list():
+    all_files = [f for f in os.listdir(MODEL_DIR) if not f.startswith('.')]
+    files = []
+    for f in all_files:
+        if '.' in f:
+            files.append(f[:f.index('.')])
+        else:
+            files.append(f)
+    return list(set(files))
 
 
 class UnknownModelException(Exception):
@@ -18,8 +30,10 @@ class UnknownModelException(Exception):
 
 
 def get_model(name):
-    if name == MODEL_GOOGLE_NEWS:
+    if name == MODEL_GOOGLE_NEWS_SHORTNAME:
         return _load_model(MODEL_GOOGLE_NEWS)
+    if name in model_name_list():
+        return _load_model(name)
     raise UnknownModelException(name)
 
 
@@ -27,7 +41,10 @@ def _load_model(name):
     if name not in model_cache:
         logger.info("Loading pre-trained word to vec model named {}...".format(name))
         path_to_model = _path_to_model(name)
-        model = gensim.models.keyedvectors.KeyedVectors.load_word2vec_format(path_to_model, binary=True)
+        if name.endswith('.bin'):  # the google one was trained in C and saved as a binary
+            model = gensim.models.keyedvectors.KeyedVectors.load_word2vec_format(path_to_model, binary=True)
+        else:  # our models are saved with a call to save, so use `load` to load them
+            model = gensim.models.keyedvectors.KeyedVectors.load(path_to_model)
         logger.info("  loaded")
         model_cache[name] = model
     return model_cache[name]
--- server/views.py
+++ server/views.py
@@ -1,13 +1,25 @@
 import logging
-from flask import request, jsonify
+from flask import request, jsonify, render_template
 import string
 from sklearn.decomposition import PCA
 
 from server import app, VERSION
 from request import form_fields_required
-from models import get_model
+from models import get_model, model_name_list
 
 logger = logging.getLogger(__name__)
+
+
+@app.route('/', methods=['GET'])
+def model_list():
+    model_names = model_name_list()
+    return render_template('index.html', models=model_names)
+
+
+@app.route('/models.json', methods=['GET'])
+def model_list_json():
+    model_names = model_name_list()
+    return jsonify(model_names)
 
 
 @app.route('/embeddings/2d.json', methods=['POST'])
@@ -15,6 +27,7 @@ logger = logging.getLogger(__name__)
 def embeddings_2d():
     word_vectors = get_model(request.form['model'])
     words = request.form.getlist('words[]')
+    words = [w for w in words if len(w) > 0]    # do a little extra cleanup - no empty words
 
     # Remove words that are not in model vocab
     words_in_model = []
@@ -31,10 +44,15 @@ def embeddings_2d():
     # reduce to a 2d representation for charting purposes
     embeddings = [word_vectors[word] for word in [w['cleaned_word'] for w in words_in_model]]
     pca = PCA(n_components=2)
-    two_d_embeddings = pca.fit_transform(embeddings).tolist()
+    try:
+        two_d_embeddings = pca.fit_transform(embeddings).tolist()
+    except ValueError:
+        # means there aren't any input words actually in the model
+        two_d_embeddings = []
     words_with_model_info = []
-    if len(words_in_model) != len(two_d_embeddings):    # safety check before processing
-        logger.warn("Number of results from model didn't match input length - maybe foreign words?")
+    if len(words_in_model) != len(two_d_embeddings):
+        # this is not crazy for a topic model, so just debug log it
+        logger.debug("Number of results from model didn't match input length - maybe foreign words?")
     else:
         for i in range(len(words_in_model)):
             words_with_model_info.append({'word': words_in_model[i]['word'], 'x': two_d_embeddings[i][0], 'y': two_d_embeddings[i][1]})
@@ -56,7 +74,11 @@ def embeddings_2d():
 @app.route('/embeddings/<word>/similar-words.json', methods=['GET'])
 def embeddings_similar_words(word):
     word_vectors = get_model(request.args.get('model'))
-    similar_words = word_vectors.most_similar(word)
+    try:
+        similar_words = word_vectors.most_similar(word)
+    except KeyError:
+        # means the word is not in the vocabulary we are using
+        similar_words = []
 
     results = []
     for sim_word in similar_words:
@@ -64,5 +86,6 @@ def embeddings_similar_words(word):
 
     return jsonify({
         'results': results,
+        'word': word,
         'version': VERSION
     })
