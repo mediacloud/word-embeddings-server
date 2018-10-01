85187f9df029224fcb73efed035a4c779e655d7f
Rahul B <rahulbot@gmail.com>
2018-10-01 14:13:09 -0400

    Version bump for release

--- server/__init__.py
+++ server/__init__.py
@@ -9,7 +9,7 @@ import mediacloud
 
 from config import get_default_config, ConfigException
 
-VERSION = "2.2.0"
+VERSION = "2.2.1"
 
 base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
